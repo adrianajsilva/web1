aee275f31fde5ed0f12b67dc2b736637716eb7c8
tatianapaixao <tatianapaixao@10.0.0.104>
2020-09-28 17:59:13 -0300

    Versao inicial

---
+++ scel/src/main/java/com/fatec/scel/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.fatec.scel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class HomeController {
+	@GetMapping("/login")
+    public String autenticacao() {
+                   return "/paginaLogin";
+    }
+	@GetMapping("/")
+	public String home() {
+		return "/paginaMenu";
+	}
+
+	@GetMapping("/aluno/cadastrar")
+	public String cadastrarAluno() {
+		return "/cadastrarAluno";
+	}
+
+	@GetMapping("/livro/cadastrar")
+	public String cadastrarLivro() {
+		return "/cadastrarLivro";
+	}
+}
